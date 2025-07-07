8c63a0a669a42e3aaae7eb374135a4a50067bc0f
nehajain-metyis <neha.jain@metyis.com>
2025-07-07 12:58:58 +0530

    Added Order Note in Checkout Page

---
+++ extensions/checkout-order-note/src/Checkout.jsx
@@ -0,0 +1,24 @@
+import {
+  reactExtension,
+  Text,
+  useNote
+} from "@shopify/ui-extensions-react/checkout";
+
+// 1. Choose an extension target
+export default reactExtension("purchase.checkout.reductions.render-after", () => (
+  <Extension />
+));
+
+function Extension() {
+  const ordernote = useNote();
+
+  // If Note Available
+  if (ordernote) {
+    return (
+      <Text>Note : {ordernote}</Text>
+    );
+  }
+
+  // If Note Not Available
+  return null;
+}
